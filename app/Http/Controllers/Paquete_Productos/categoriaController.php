be0afce0da210e5ade82e7e5f576282ff4c17074
Vit537 <office.manuel.ab@gmail.com>
2025-05-15 15:14:06 -0400

    Cambiar fecha produto table migration

--- app/Http/Controllers/Paquete_Productos/categoriaController.php
+++ app/Http/Controllers/Paquete_Productos/categoriaController.php
@@ -29,7 +29,7 @@ class categoriaController extends Controller
 
          if (auth()->user()->tipo === 'administrador') {
             $categorias=categoria::all();
-           return view('Paquete_productos.categoria.register', compact('categoria'));
+           return view('Paquete_productos.categoria.register', compact('categorias'));
         }
         abort(403, 'No tienes permiso para crear esta categoria.');
     }
