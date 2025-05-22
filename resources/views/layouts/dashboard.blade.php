08f376e5788ed8a1068e28e5ad6eb5a47feca719
Rick Grimes <sure.pencil@gmail.com>
2025-05-22 16:22:12 -0400

    nuevo

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -158,8 +158,8 @@
 
 
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
 
 
