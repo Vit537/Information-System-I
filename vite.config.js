01a19abb7013d8be5ffa545b1d48af12b494e773
Rick Grimes <sure.pencil@gmail.com>
2025-05-22 04:21:34 -0400

    nuevo11

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-     build: {
-        outDir:	'public/build',
-	manifest: true,  // Genera manifest.json en public/build
+    build: {
+        outDir: "public/build",
+        manifest: true, // Genera manifest.json en public/build
         emptyOutDir: true,
+        assetsDir: "",
+        rollupOptions: {
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            output: {
+                assetFileNames: "assets/[name].[hash][extname]",
+                entryFileNames: "assets/[name].[hash].js",
+            },
+        },
     },
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
-
         }),
     ],
-
 });
