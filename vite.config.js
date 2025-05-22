3ffa9f13dda2a81230172412f9400abcfd959227
Rick Grimes <sure.pencil@gmail.com>
2025-05-22 15:47:14 -0400

    t

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    base: '/build/', // ¡Clave para Railway!
+    // base: '/build/', // ¡Clave para Railway!
     build: {
         outDir: 'public/build',
         manifest: true,
