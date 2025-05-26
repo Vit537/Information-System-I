e0a8068c6c797774c64fd5c5c242fd777a3da5b8
Rick Grimes <sure.pencil@gmail.com>
2025-05-26 04:04:04 -0400

    node

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+
     @vite('resources/css/app.css')
     {{-- {{-- script de livewire --}}
     @livewireStyles
