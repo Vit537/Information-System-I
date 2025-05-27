8dd328ee0ab0c7bae67fdb3f8ffb93b558c81e0b
Rick Grimes <sure.pencil@gmail.com>
2025-05-27 03:28:43 -0400

    listo

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -16,7 +16,7 @@
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
     {{-- {{-- script de livewire --}}
     @livewireStyles
-    @vite('resources/css/app.css','resources/js/app.js')
+   @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
