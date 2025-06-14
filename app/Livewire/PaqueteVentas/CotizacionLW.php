fe2576be4e3d745c42c6529d42e541551c5d45db
Vit537 <office.manuel.ab@gmail.com>
2025-06-13 20:13:16 -0400

    Añadir filtros a Cotizacion y Ventas

--- app/Livewire/PaqueteVentas/CotizacionLW.php
+++ app/Livewire/PaqueteVentas/CotizacionLW.php
@@ -5,21 +5,45 @@ namespace App\Livewire\PaqueteVentas;
 use Livewire\Component;
 use App\Models\Paquete_productos\producto;
 use App\Models\Paquete_Ventas\cotizacion;
-use App\Models\Paquete_Ventas\cotizacion_detalle;
+use App\Models\Paquete_Usuarios\Auth\Persona;
+
+use function PHPUnit\Framework\isNull;
 
 class CotizacionLW extends Component
 {
     public $count = 0;
+    public $cliente_id = null;
+    public $empleado_id = null;
+    public $monto_min = 0;
+    public $monto_max = PHP_INT_MAX;
+    public $clientes;
+    public $empleados;
     
     public function render()
     {
-        return view('livewire.paquete-ventas.cotizacion.cotizacion',[
-            'cotizaciones' => $this->getCotizaciones()
-        ]);
+        return view('livewire.paquete-ventas.cotizacion.cotizacion');
+    }
+
+    public function mount(){
+        $this->clientes = persona::where('tipo', 'cliente')->get();
+        $this->empleados = persona::where('tipo','usuario')->orWhere('tipo','administrador')->get();
     }
 
     public function getCotizaciones(){
-        return cotizacion::all();
+        $cotizaciones = cotizacion::all();
+        if($this->cliente_id){
+            $cotizaciones = $cotizaciones->where('cliente_id', $this->cliente_id);
+        }
+        if($this->empleado_id){
+            $cotizaciones = $cotizaciones->where('empleado_id', $this->empleado_id);
+        }
+        if($this->monto_min){
+            $cotizaciones = $cotizaciones->where('monto_total', '>=', $this->monto_min);
+        }
+        if($this->monto_max){
+            $cotizaciones = $cotizaciones->where('monto_total', '<=', $this->monto_max);
+        }
+        return $cotizaciones;
     }
 
     public function deleteCotizacion($id){
