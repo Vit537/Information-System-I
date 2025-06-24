291b661971fb0c2be000ee9a69589d726c7aa74a
Rick Grimes <sure.pencil@gmail.com>
2025-06-23 20:39:58 -0400

    coma2

--- app/Livewire/ReporteCompra.php
+++ app/Livewire/ReporteCompra.php
@@ -4,10 +4,222 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+
+
+use App\Models\Paquete_compra\ordenCompra;
+use Livewire\Attributes\On;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Illuminate\Support\Facades\Auth;
+use PhpOffice\PhpSpreadsheet\RichText\RichText;
+
 class ReporteCompra extends Component
 {
+     public $datos = [];
+    public $mensaje = null;
+
+    public $modoEdicion = false;
+    public $item_id;
+    public $nombre;
+    public $correo;
+
+    //search and filter
+
+    public $busqueda = '';
+    public $precio_min;
+    public $precio_max;
+    public $fecha_inicio;
+    public $fecha_fin;
+    public $orden = 'asc'; // asc o desc
+    public $ordenP = 'asc'; // asc o desc
+
     public function render()
     {
-        return view('livewire.reporte-compra');
+        return view('livewire.reporte-compra', [
+            'proveedores' =>  $this->getProveedor()
+        ]);
+    }
+
+      public function getProveedor()
+    {
+        // $ordenes = ordenCompra::with('proveedor.persona');
+        $ordenes = OrdenCompra::with([
+            'proveedor.persona',
+            'productos' // Aquí cargas la relación muchos a muchos
+        ]);
+
+
+
+        if ($this->fecha_inicio) {
+            $ordenes->whereDate('created_at', '>=', $this->fecha_inicio);
+        }
+
+        if ($this->fecha_fin) {
+            $ordenes->whereDate('created_at', '<=', $this->fecha_fin);
+        }
+        // dd($ordenes->get());
+        // Ordenar por precio
+        $ordenes->orderBy('created_at', $this->orden);
+
+        // $ordenes->orderBy('precio_total', $this->ordenP);
+
+
+        if ($this->busqueda !== '') {
+
+            $ordenes->whereHas('proveedor.persona', function ($q) {
+                $q->where('nombre', 'ILIKE', '%' . $this->busqueda . '%');
+            });
+        }
+
+        //    $te = $ordenes->get();
+        //     dd($te);
+        return $ordenes->get();
+
+        // return ordenCompra::with('proveedor.persona')->get();
+    }
+
+    public function bold($dato1, $dato2)
+    {
+        // Crear texto enriquecido
+        $richText = new RichText();
+        $boldPart = $richText->createTextRun($dato1);
+        $boldPart->getFont()->setBold(true); // negrita
+        $richText->createText($dato2); // anade mas texto pero normal no negrita
+        return $richText;
+    }
+
+
+    public function exportarTareas()
+    {
+        $data = $this->getProveedor();
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Título
+        $sheet->mergeCells('A1:F1');
+        $sheet->setCellValue('A1', 'Reporte De Compras');
+        $sheet->getStyle('A1')->getFont()->setSize(18)->setBold(true)->getColor()->setARGB('993333');
+        $sheet->getStyle('A1')->getAlignment()->setHorizontal('center');
+
+        //descripcion de encabezado
+        $sheet->mergeCells('A3:D3');
+        $sheet->mergeCells('A4:D4');
+        $sheet->mergeCells('A5:D5');
+        $nombre = Auth::user()->nombre;
+
+        // Establecer en celda A3
+        $tittle1 = $this->bold('Usuario que solicita el informe: ', $nombre);
+        $sheet->setCellValue('A3', $tittle1);
+
+
+
+        $tittle2 = $this->bold('Cliente: ', $data[0]->proveedor->persona->nombre);
+        $sheet->setCellValue('A4', $tittle2);
+        // $sheet->setCellValue('A4', "Cliente: {$data[0]->proveedor->persona->nombre}");
+
+
+        if ($this->fecha_inicio !== null && $this->fecha_fin !== null) {
+
+            $tittle3 = $this->bold('Rango de fechas: ', $this->fecha_inicio . ' hasta ' . $this->fecha_fin);
+
+
+            $sheet->setCellValue('A5', $tittle3);
+        } else {
+            $tittle3 = $this->bold('Rango de fechas: ', '...son todas las compras!');
+
+
+            $sheet->setCellValue('A5', $tittle3);
+            // $sheet->setCellValue('A5', 'Rango de fechas: ...son todas las compras!');
+        }
+        $sheet->getStyle('A3:A5')->getFont()->setSize(12)->getColor()->setARGB('070808');
+
+
+        // Encabezados
+        $encabezados = [
+            'Nroº Compra',
+            'Nombre Proveedor/Empresa',
+            'Fecha/Hora',
+            'Direccion',
+            'Telefono',
+            'Precio Total',
+        ];
+
+        $col = 'A';
+        foreach ($encabezados as $header) {
+            $sheet->setCellValue($col . '7', $header);
+            $sheet->getColumnDimension($col)->setAutoSize(true);
+            $sheet->getStyle($col . '7')->getFont()->setBold(true)->getColor()->setARGB('FFFFFF');
+            $sheet->getStyle($col . '7')->getFill()->setFillType('solid')->getStartColor()->setARGB('004E60'); // azul oscuro
+            $sheet->getStyle($col . '7')->getAlignment()->setHorizontal('center');
+            $col++;
+        }
+
+
+        $startRow = 8;
+        $contador = 0;
+
+        foreach ($data as $i) {
+            $row = $startRow + $contador;
+            $color = ($contador % 2 == 0) ? 'D9EEF3' : 'FFFFFF'; // celeste / blanco
+
+            $sheet->getStyle("A{$row}:F{$row}")->getFill()
+                ->setFillType('solid')
+                ->getStartColor()->setARGB($color);
+
+            //     // Rellenar columnas
+
+            $sheet->setCellValue("A{$row}", $i->id);
+            $sheet->setCellValue("B{$row}", $i->proveedor->persona->nombre);
+            $sheet->setCellValue("C{$row}", $i->created_at);
+            $sheet->setCellValue("D{$row}", $i->proveedor->persona->direccion);
+            $sheet->setCellValue("E{$row}", $i->proveedor->persona->telefono);
+            $total = 0;
+            foreach ($i->productos as $prod) {
+                $total += $prod->pivot->cantidad * $prod->pivot->precio_unitario;
+            }
+            $sheet->setCellValue("F{$row}", $total);
+
+
+
+            $contador += 1;
+        }
+        $row = $startRow + $contador - 1;
+        $sheet->getStyle("A8:F{$row}")
+            ->getAlignment()
+            ->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+        $sheet->getStyle("A8:F{$row}")
+            ->getAlignment()
+            ->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
+
+
+        // Descargar el archivo
+        $fileName = 'ReporteCompras.xlsx';
+        $tempFile = tempnam(sys_get_temp_dir(), $fileName);
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save($tempFile);
+
+        return response()->download($tempFile, $fileName)->deleteFileAfterSend(true);
+    }
+
+    public function exportarPDF()
+    {
+        $datos = $this->getProveedor(); // o la tabla completa que tenés
+        session(['datos_pdf' => $datos]);
+
+        $this->js("window.dispatchEvent(new CustomEvent('descargar-pdf'))");
+    }
+
+    public function imprimir()
+    {
+        $datos = $this->getProveedor(); // o la tabla completa que tenés
+        session(['datos_pdf' => $datos]);
+
+        $this->js("window.dispatchEvent(new CustomEvent('imprimir'))");
     }
 }
+
+
+
--- resources/views/livewire/reporte-compra.blade.php
+++ resources/views/livewire/reporte-compra.blade.php
@@ -1,3 +1,211 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
-</div>
+ <div class="px-10 ">
+
+     @if (session()->has('mensaje'))
+         <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
+             class="bg-green-100 border border-green-400 text-green-700 px-4 py-2 rounded relative mb-4">
+             {{ session('mensaje') }}
+         </div>
+     @endif
+
+
+
+     <div class="flex justify-between py-2">
+
+
+         <div class="gap-4 flex w-full flex-col md:flex-row  justify-center items-center py-2">
+             <a href="{{ route('add.compra') }}"
+                 class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                 Crear orden compra
+             </a>
+
+
+
+         </div>
+
+     </div>
+     <div class="flex gap-3 mb-4">
+         <button wire:click="imprimir" class="bg-slate-300 hover:bg-slate-200 text-white px-2  py-1 rounded-lg"><img
+                 class="w-8" src="{{ asset('assets/printer.png') }}" alt="logo"></button>
+
+         <button wire:click="exportarPDF" class="bg-slate-300 hover:bg-slate-200 text-white px-2 py-1  rounded-lg"><img
+                 class="w-8" src="{{ asset('assets/image.png') }}" alt="logo"></button>
+
+         <button wire:click="exportarTareas"
+             class="bg-slate-300 hover:bg-slate-200 text-white px-2 py-0 rounded-lg "><x-svg.excel /></button>
+     </div>
+
+
+
+
+
+
+     <div class="  mb-4">
+
+         <label for="search" class="block text-base font-medium text-slate-100 dark:text-slate-600 ">Buscador</label>
+         <input id="search" type="text" wire:model.live="busqueda" placeholder="Buscar personas por nombre..."
+             class="mt-1 border p-2 rounded w-full md:w-1/2  mb-4">
+
+
+         <div x-data="{ abierto: false }" class="w-full mb-4">
+             {{-- md:hidden --}}
+             {{-- <!-- Botón para mostrar/ocultar --}}
+             <div class="flex justify-start items-start md:w-1/4">
+                 <button @click="abierto = !abierto"
+                     class=" w-full hover:bg-slate-900 dark:hover:bg-slate-300 text-white py-2 px-4 rounded mb-2 border-b-2 border-slate-500">
+                     <span class="text-slate-300 dark:text-slate-900" x-show="!abierto"><i class="fa fa-filter"
+                             aria-hidden="true"></i> Mostrar filtros</span>
+                     <span class="text-slate-300 dark:text-slate-900" x-show="abierto"><i class="fa fa-filter"
+                             aria-hidden="true"></i> Ocultar filtros</span>
+                 </button>
+             </div>
+
+             {{-- <!-- Contenedor de filtros  --}}
+             {{-- <div x-show="abierto || window.innerWidth >= 768" x-transition class="space-y-4"> --}}
+             <div x-show="abierto " x-transition class="space-y-4">
+
+                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                     <div>
+                         <label class="block text-base font-medium text-slate-100 dark:text-slate-600 ">Fecha
+                             desde</label>
+                         <input wire:model.live="fecha_inicio" type="date" class="border p-2 rounded w-full">
+                     </div>
+                     <div>
+                         <label class="block text-base font-medium text-slate-100 dark:text-slate-600 ">Fecha
+                             hasta</label>
+                         <input wire:model.live="fecha_fin" type="date" class="border p-2 rounded w-full">
+                     </div>
+                 </div>
+
+                 <div>
+                     <label class="block text-base font-medium text-slate-100 dark:text-slate-600 ">Ordenar por
+                         Fecha</label>
+                     <select wire:model.live="orden" class="border p-2 rounded w-full md:w-1/2">
+                         <option value="asc">Menor a mayor</option>
+                         <option value="desc">Mayor a menor</option>
+                     </select>
+                 </div>
+
+             </div>
+         </div>
+
+
+     </div>
+
+
+
+
+
+     <div class="relative overflow-x-auto shadow-md sm:rounded-lg p-2 bg-white">
+         {{-- @if (strlen($busqueda) > 0) --}}
+         {{-- <p>no entra a ningun lado</p> --}}
+
+         @if (count($proveedores) > 0)
+             <table
+                 class="w-full text-sm text-left text-gray-800 dark:text-black border-collapse bg-zinc-100 dark:bg-zinc-300 dark:border-s-gray-400
+                ">
+                 <thead
+                     class="text-xs text-black dark:text-gray-800 uppercase bg-gray-200 dark:bg-slate-400 border-b-4 border-gray-400 dark:border-gray-500 ">
+
+                     <tr>
+                         <th scope="col" class="px-6 py-3 text-center">
+                             <h4>Nroº compra</h4>
+                         </th>
+                         <th scope="col" class="px-6 py-3 text-center">
+                             <h4>Nombre</h4>
+                             <h5>Proveedor/Empresa</h5>
+                         </th>
+                         <th scope="col" class="px-6 py-3 text-center">
+                             <h4>Fecha/Hora</h4>
+                         </th>
+                         <th scope="col" class="px-6 py-3 text-center">
+                             <h4>direccion</h4>
+                         </th>
+                         <th scope="col" class="px-6 py-3 text-center">
+                             <h4>telefono</h4>
+                         </th>
+                         <th scope="col" class="px-6 py-3 text-center">
+                             <h4>Precio Total</h4>
+                         </th>
+                         {{-- <th scope="col" class="px-6 py-3">
+                            <h4>Ver detalle/factura</h4>
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            <h4>Acciones</h4>
+                        </th> --}}
+
+                     </tr>
+                 </thead>
+                 <tbody>
+                     {{-- contenido --}}
+                     @foreach ($proveedores as $proveedor)
+                         <tr>
+
+                             <td class="border-b-2 border-gray-300 dark:border-gray-400 text-center w-16">
+                                 {{ $proveedor->id }}
+                             </td>
+                             <td class="border-b-2 border-gray-300 dark:border-gray-400 p-1 ">
+                                 <div class="font-semibold text-base text-gray-800 dark:text-gray-800">
+                                     {{ $proveedor->proveedor->persona->nombre }}</div>
+                                 <div class="text-sm text-gray-600 dark:text-gray-600">
+                                     {{ $proveedor->proveedor->persona->correo }}</div>
+                             </td>
+
+
+                             <td class="border-b-2 border-gray-300 dark:border-gray-400 text-center">
+                                 {{ $proveedor->created_at }}
+                             </td>
+                             <td class="border-b-2 border-gray-300 dark:border-gray-400 text-center">
+                                 {{ $proveedor->proveedor->persona->direccion }}
+                             </td>
+                             <td class="border-b-2 border-gray-300 dark:border-gray-400 text-center">
+                                 {{ $proveedor->proveedor->persona->telefono }}
+                             </td>
+                             <td class="border-b-2 border-gray-300 dark:border-gray-400 text-center">
+                                 @php
+                                     $total = 0;
+                                     $contador = 1;
+                                 @endphp
+                                 @foreach ($proveedor['productos'] as $prod)
+                                     @php
+                                         $total += $prod->pivot->cantidad * $prod->pivot->precio_unitario;
+                                         $contador += 1;
+                                         // $total += $producto['cantidad'] * $producto['precio'];
+                                     @endphp
+                                 @endforeach
+
+                                 <p> $ {{ number_format($total, 2) }}</p>
+
+                             </td>
+
+
+
+
+
+
+
+
+                         </tr>
+                     @endforeach
+
+
+
+                 </tbody>
+             </table>
+         @else
+             <p class="text-gray-500 bg-white">No se encontraron resultados para "{{ $busqueda }}".</p>
+         @endif
+
+     </div>
+
+     {{-- scripts --}}
+     <script>
+         window.addEventListener('descargar-pdf', function() {
+             window.open("{{ route('pdf.reporte-compra') }}", '_blank');
+         });
+
+         window.addEventListener('imprimir', function() {
+             window.open("{{ route('imprimir-compra') }}", '_blank');
+         });
+     </script>
+
+ </div>
